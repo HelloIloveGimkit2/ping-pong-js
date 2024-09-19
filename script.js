1d50a40c7c96ac2ffe01a77859b040ccedc9fad8
HelloIloveGimkit2 <eliaspitre@protonmail.com>
2024-09-19 09:14:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,156 @@
+// global vars
+var DIRECTION = {
+  IDLE: 0,
+  UP: 1,
+  DOWN: 2,
+  LEFT: 3,
+  RIGHT: 4,
+};
+
+var rounds = [5, 5, 3, 3, 2];
+var clolors = ["#1abc9c", "#2ecc71", "3498db", "#8c52ff", "#9b59b6"];
+
+// the ball object
+
+var Ball = {
+  new: function (incrementedSpeed) {
+    return {
+      width: 18,
+      height: 18,
+      x: this.canvas.width / 2 - 9,
+      y: this.canvas.height / 2 - 9,
+      moveX: DIRECTION.IDLE,
+      moveY: DIRECTION.IDLE,
+      speed: incrementedSpeed || 7,
+    };
+  },
+};
+
+// the ai object
+var Ai = {
+  new: function (side) {
+    return {
+      width: 18,
+      height: 180,
+      x: side === "left" ? 150 : this.canvas.width - 150,
+      y: this.canvas.height / 2 - 35,
+      score: 0,
+      move: DIRECTION.IDLE,
+      speed: 8,
+    };
+  },
+};
+
+var Game = {
+  initialize: function () {
+    this.canvas = document.querySelector("canvas");
+    this.context = this.canvas.getContext("2d");
+
+    this.canvas.width = 1400;
+    this.canvas.height = 1000;
+
+    this.canvas.style.width = this.canvas.width / 2 + "px";
+    this.canvas.style.height = this.canvas.height / 2 + "px";
+
+    this.player = Ai.new.call(this, "left");
+    this.ai = Ai.new.call(this, "right");
+    this.ball = Ball.new.call(this);
+
+    this.ai.speed = 5;
+    this.running = this.over = false;
+    this.turn = this.ai;
+    this.timer = this.round = 0;
+    this.color = "#8c52ff";
+
+    Pong.menu();
+    Pong.listen();
+  },
+
+  endGameMenu: function (text) {
+    Pong.context.font = "45px Courier New";
+    Pong.context.fillStyle = this.color;
+
+    Pong.context.fillRect(
+      Pong.canvas.width / 2 - 350,
+      Pong.canvas.height / 2 - 48,
+      700,
+      100
+    );
+
+    Pong.context.fillStyle = "#ffffff";
+
+    // Draw the end game menu (gameover)
+    Pong.context.fillText(
+      text,
+      Pong.canvas.width / 2,
+      Pong.canvas.height / 2 + 15
+    );
+
+    setTimeout(function () {
+      Pong = Object.assign({}, Game);
+      Pong.initialize();
+    }, 3000);
+  },
+
+  menu: function () {
+    // Draw all of the pong objects in their state
+
+    Pong.draw();
+
+    this.context.font = "50px Courier New";
+    this.context.fillStyle = this.color;
+
+    // Draw the rectangle behind the 'press any key to being' text.
+
+    this.context.fillRect(
+      this.canvas.width / 2 - 350,
+      this.canvas.height / 2 - 48,
+      700,
+      100
+    );
+
+    this.context.fillStyle = "#ffffff";
+
+    this.context.fillText(
+      "Press any key to being",
+      this.canvas.width / 2,
+      this.canvas.height / 2 + 15
+    );
+  },
+
+  update: function () {
+    if (!this.over) {
+      if (this.ball.x <= 0) Pong._resetTurn.call(this, this.ai, this.player);
+      if (this.ball.x >= this.canvas.width - this.ball.width)
+        Pong._resetTurn.call(this, this.player, this.ai);
+      if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
+      if (this.ball.y >= this.canvas.height - this.ball.height) this.ball.moveY = DIRECTION.UP;
+
+      //Move player if their player.move value was updated by a key press
+      if (this.playedr.move === DIRECTION.UP) this.player.y -= this.player.speed;
+      else if (this.player.move === DIRECTION.DOWN) this.player.y += this.player.speed;
+
+
+
+      if (Pong._turnDelayIsOver.call(this) && this.turn) {
+        this.ball.moveX = this.turn === this.player ? DIRECTION.LEFT : DIRECTION.RIGHT;
+        this.ball.moveY = [DIRECTION.UP, DIRECTION.DOWN] [Math.round(Math.random())];
+        this.ball.y = Math.floor(Math.random() * this.canvas.height - 200) + 200;
+        this.turn = null;
+      }
+
+      // if the player collides with the bound limits update the x and y coords.
+      if (this.player.y <= 0) this.player.y = 0;
+      else if (this.player.y >= (this.canvas.height - this.player.height)) this.player.y = {this.canvas.height - this.player.height);
+
+      // Move ball in intended direction based on moveY and moveX value
+      if (this.ball.moveY === DIRECTION.UP) this.ball.y -= (this.ball.speed / 1.5);
+      else if (this.ball.moveY === DIRECTION.DOWN) this.ball.y += (this.ball.speed / 1.5);
+      if (this.ball.moveX === DIRECTION.LEFT) this.ball.x -= this.ball.speed;
+      else if (this.ball.moveX === DIRECTION.RIGHT) this.ball.x += this.ball.speed;
+
+      // Hand the ai up and down movement
+      
+    }
+  },
+};
