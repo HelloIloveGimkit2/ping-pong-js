dfe71b74d22979f6bbe66b04a22c97739f06ef1a
HelloIloveGimkit2 <eliaspitre@protonmail.com>
2024-09-23 14:23:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var Ball = {
       y: this.canvas.height / 2 - 9,
       moveX: DIRECTION.IDLE,
       moveY: DIRECTION.IDLE,
-      speed: incrementedSpeed || 10,
+      speed: incrementedSpeed || 18,
     };
   },
 };
